b0805aa14bf829714f24171abaa17d351d983cde
teaguehall <teaguehall11@gmail.com>
2022-01-27 22:37:14 -0600

    added exec call after fork

--- hw1/aggregator.c
+++ hw1/aggregator.c
@@ -5,8 +5,11 @@
 void main(int argc, char* argv[])
 {
     int producer, max_producers;
+    pid_t aggregator_pid;
     pid_t producer_pids[5]; // allocate for max number of producers
-    char* producer_args[]= { "./producer.out", NULL};
+    char str_aggregator_pid[1024];
+    char str_producer_id[1024];
+    char* producer_args[]= { "./producer.out", str_aggregator_pid, str_producer_id};
 
     // confirm correct numbers of args
     if(argc != 2)
@@ -23,6 +26,10 @@ void main(int argc, char* argv[])
         exit(EXIT_FAILURE);
     }
 
+    // write aggregator pid to arg vector
+    sprintf(str_aggregator_pid, "%u", getpid());
+
+
     // create producers
     for(producer = 0; producer < max_producers; producer++)
     {
@@ -31,9 +38,12 @@ void main(int argc, char* argv[])
         // create producer process
         if(producer_pids[producer] == 0)
         {
+            // write current producer ID to arg vector
+            sprintf(str_producer_id, "%u", producer);
+            
             execvp(producer_args[0], producer_args);
         }
-        
+
     }
 
     sleep(1);
--- hw1/producer.c
+++ hw1/producer.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 void main(int argc, char* argv[0])
 {
-    printf("Hello! I'm producer!\n");
+    pid_t aggregator_pid;
+    int producer_id;
+    
+    // confirm number of arguments
+    if(argc != 3)
+    {
+        printf("ERROR: Please specify aggregator process ID and producer ID. Exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // grab aggregator PID
+    aggregator_pid = atoi(argv[1]);
+    if(aggregator_pid)
+    {
+        printf("ERROR: Please specify aggregator process ID and producer ID. Exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // grab producer ID
+    producer_id = atoi(argv[2]); // TODO - should we confirm valid producer ID?
+    
+    printf("Hello from producer %d!\n", producer_id);
     return;
 }
