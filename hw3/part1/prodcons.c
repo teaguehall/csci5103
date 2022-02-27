2fba1618f6c67dcb58cd684addf82b2a9bc0ecae
teaguehall <teaguehall11@gmail.com>
2022-02-27 00:43:13 -0600

    Added producer -> modifier signaling

--- hw3/part1/prodcons.c
+++ hw3/part1/prodcons.c
@@ -3,8 +3,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <error.h>
+#include <errno.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <sys/time.h>
 
 // globals
 
@@ -21,14 +23,20 @@ pthread_t tid_producer;
 pthread_t tid_modifier;
 pthread_t tid_consumer;
 
-// buffer stuff
+// buffer items
 typedef struct Item
 {
     int id;
     char timestamp[128];
 } Item;
 
-int buffer_size;
+Item* item_buffer;
+size_t max_items;
+
+// buffer indexes 
+int next_index_producer = 0;
+int next_index_modifier = 0;
+int next_index_consumer = 0;
 
 // semaphores
 sem_t sem_buffer_free;
@@ -46,11 +54,18 @@ int main(int argc, char* argv[])
         exit(EXIT_FAILURE);
     }
 
-    // set buffer size
-    buffer_size = atoi(argv[1]);
-    if(buffer_size <= 0)
+    // set item buffer size
+    max_items = atoi(argv[1]);
+    if(max_items <= 0)
     {
         throwError(0, "Invalid buffer size specified");
+    }
+
+    // allocate memory for item buffer
+    item_buffer = malloc(max_items * sizeof(Item));
+    if(item_buffer == NULL)
+    {
+        throwError(0, "Failed to allocate memory for item buffer");
     }
 
     // set producer size if specifed
@@ -63,7 +78,10 @@ int main(int argc, char* argv[])
         }
     }
 
-    // allocate memory for item buffer
+    // initialize semaphores
+    sem_init(&sem_buffer_free, 0, max_items);
+    sem_init(&sem_to_be_modified, 0, 0);
+    sem_init(&sem_to_be_consumed, 0, 0);
 
     // spawn threads
     pthread_create(&tid_producer, NULL, routineProducer, NULL);
@@ -94,18 +112,87 @@ void throwError(int errnum, char* message)
 
 void* routineProducer(void* vargp)
 {
-    int item;
-    for(item = 0; item < produced_items; item++)
-    {
-        printf("Produced item %d\n", item);
-        sleep(1);
-    }
+    Item* pitem;
+    struct timeval timestamp;
     
+    // create log file
+    FILE* log = fopen("producer.log", "w");
+    if(log == NULL)
+    {
+        throwError(0, "Failed to create producer.log");
+    }
+
+    // send produced items to buffer 
+    for(int i = 0; i < produced_items; i++)
+    {
+        sleep(1);
+        
+        // wait until there is room in the buffer
+        sem_wait(&sem_buffer_free);
+
+        // grab time of day
+        if(gettimeofday(&timestamp, NULL) == -1)
+        {
+            throwError(errno, "Failed to retrieve timestamp");
+        }
+
+        // grab next available item slot from buffer
+        pitem = item_buffer + next_index_producer;
+
+        // write to buffer
+        pitem->id = i;
+        sprintf(pitem->timestamp, "%ld-%ld", timestamp.tv_sec, timestamp.tv_usec);
+
+        // signal to modifier
+        sem_post(&sem_to_be_modified);
+
+        // increment next producer index
+        next_index_producer++;
+        if(next_index_producer >= max_items)
+        {
+            next_index_producer = 0;
+        }
+
+        // log to file
+        fprintf(log, "%d %s\n", pitem->id, pitem->timestamp);
+        fflush(log);
+    }
+
+    // send EOS signal
+    pitem = item_buffer + next_index_producer;
+    sprintf(pitem->timestamp, "EOS");
+    sem_post(&sem_to_be_modified);
 }
 
 void* routineModifier(void* vargp)
 {
-    printf("Hello from modifier!\n");
+    Item* pitem;
+    
+    // run indefinitely until EOS received
+    while(1)
+    {
+        // wait until to be modified signal
+        sem_wait(&sem_to_be_modified);
+
+        printf("Modifier received value!\n");
+
+        // update item pointer
+        pitem = item_buffer + next_index_modifier;
+
+        // update next modifier index
+        next_index_modifier++;
+        if(next_index_modifier >= max_items)
+        {
+            next_index_modifier = 0;
+        }
+
+        // check if EOS
+        if(strcmp(pitem->timestamp, "EOS") == 0)
+        {
+            printf("Modifier received EOS\n");
+            break;
+        }
+    }
 }
 
 void* routineConsumer(void* vargp)
